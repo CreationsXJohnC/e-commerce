d84a0d57565e63462fcb8ca3e68df3084bc04d14
Che <johnccreations21@gmail.com>
2025-07-30 11:51:34 -0400

    initial commit

---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,64 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Price from "../components/ui/Price";
+import Rating from "../components/ui/Rating";
+
+const BookInfo = ({ books }) => {
+    return (
+        <div id="books__body">
+            <main id="books__main">
+                <div className="books__container">
+                    <div className="row">
+                        <div className="book__selected--top">
+                            <Link to="/books" className="book__link">
+                                <FontAwesomeIcon icon="arrow-left" />
+                            </Link>
+                            <Link to="/books" className="book__link">
+                                <h2 className="book__selected--title--top">Books</h2>
+                            </Link>
+                        </div>
+                        <div className="book__selected">
+                            <figure className="book__selected--figure">
+                                <img src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg" alt="" className="book__selected--img" />
+                            </figure>
+                            <div className="book__selected--description">
+                                <h2 className="book__selected--title">Crack the coding interview</h2>
+                                <Rating rating="4.5" />
+                                <div className="book__selected--price"></div>
+                                    <Price originalPrice={50} salePrice={20} />
+                            </div>
+                            <div className="book__summary">
+                                <div className="book__summary--title">
+                                    Summary
+                                </div>
+                                <p className="book__summary__para">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi temporibus culpa ut rem architecto vitae porro doloribus. Sint, quidem! Fugiat incidunt dignissimos labore a odit sint, aperiam ea sed dicta.
+                                </p>
+                                <p className="book__summary__para">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi temporibus culpa ut rem architecto vitae porro doloribus. Sint, quidem! Fugiat incidunt dignissimos labore a odit sint, aperiam ea sed dicta.
+                                </p>
+                            </div>
+                            <button className="btn">
+                                Add to cart
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="books__container">
+                    <div className="row">
+                        <div className="book__selected--title--top">
+                            <h2 className="book__selected--title--top">
+                                Recommended Books
+                            </h2>
+                        </div>
+                        
+                    </div>
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default BookInfo;
